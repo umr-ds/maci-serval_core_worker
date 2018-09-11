5b8a85e0491bbc2b8ca536e97251edcc8694ebc8
Jonas Höchst <git@jonashoechst.de>
2018-09-11 16:23:49 +0200

    fixed keyring dump generation for older serval releases

--- dotcore/myservices/serval.py
+++ dotcore/myservices/serval.py
@@ -10,31 +10,41 @@ import nacl.hash
 from nacl.encoding import HexEncoder
 from nacl.bindings import crypto_sign_seed_keypair, crypto_sign_ed25519_sk_to_curve25519, crypto_scalarmult_base
 
-servald_keyring_dump_format = '''0: type=0x04(DID)  DID="{}" Name="{}"
-0: type=0x03(RHIZOME)  sec={}
-0: type=0x06 (CRYPTOCOMBINED)  pub={} sec={}
+sevald_keyring_dump_format_legacy = '''0: type=4(DID)  DID="{did}" Name="{name}"
+0: type=1(CRYPTOBOX)  pub={box_pk} sec={box_sk}
+0: type=2(CRYPTOSIGN)  pub={sign_pk} sec={sign_sk}
+0: type=3(RHIZOME)  sec={rhiz_pk}
+'''
+
+servald_keyring_dump_format = '''0: type=0x04(DID)  DID="{did}" Name="{name}"
+0: type=0x06 (CRYPTOCOMBINED)  pub={sign_pk}{box_pk} sec={sign_sk}{box_sk}
+0: type=0x03(RHIZOME)  sec={rhiz_pk}
 '''
 
 def generate_serval_keys(name):
-    node_hash = HexEncoder.decode(nacl.hash.sha256(name))
-    pk_sign_key, sk_sign_key = crypto_sign_seed_keypair(node_hash)
-    sk_box_key = crypto_sign_ed25519_sk_to_curve25519(sk_sign_key)
-    pk_box_key = crypto_scalarmult_base(sk_box_key)
+    node_hash = HexEncoder.decode(nacl.hash.sha256(name.encode("utf-8")))
+    sign_pk, sign_sk = crypto_sign_seed_keypair(node_hash)
+    box_sk = crypto_sign_ed25519_sk_to_curve25519(sign_sk)
+    box_pk = crypto_scalarmult_base(box_sk)
 
-    pub = HexEncoder.encode(pk_sign_key+pk_box_key).decode("ascii").upper()
-    sec = HexEncoder.encode(sk_sign_key+sk_box_key).decode("ascii").upper()
-
-    node_hash_rhizome = HexEncoder.decode(nacl.hash.sha256("rhizome"+name))
-    rhiz = HexEncoder.encode(node_hash_rhizome).decode("ascii").upper()
-
-    return (pub, sec, rhiz)
+    rhiz_pk = HexEncoder.decode(nacl.hash.sha256(("rhizome"+name).encode("utf-8")))
+    
+    keys = {
+        "sign_pk": HexEncoder.encode(sign_pk).decode("ascii").upper(),
+        "box_sk":  HexEncoder.encode( box_sk).decode("ascii").upper(),
+        "sign_sk": HexEncoder.encode(sign_sk).decode("ascii").upper(),
+        "box_pk":  HexEncoder.encode( box_pk).decode("ascii").upper(),
+        "sid":     HexEncoder.encode( box_pk).decode("ascii").upper(),
+        "rhiz_pk": HexEncoder.encode(rhiz_pk).decode("ascii").upper(),
+    }
+    return keys
 
 def generate_serval_keyring_dump(name):
-    pub, sec, rhiz = generate_serval_keys(name)
-    node_num = "".join(c for c in name if c.isdigit()).rjust(5, "0")
+    keys = generate_serval_keys(name)
+    keys["did"] = "".join(c for c in name if c.isdigit()).rjust(5, "0")
+    keys["name"] = name
 
-    return servald_keyring_dump_format.format(node_num, name, rhiz, pub, sec)
-
+    return sevald_keyring_dump_format_legacy.format(**keys)
 
 class ServalService(CoreService):
     '''
@@ -75,8 +85,7 @@ debug.rhizome=true
             return generate_serval_keyring_dump(node.name)
 
         if filename == "serval.sid":
-            pub = generate_serval_keys(node.name)[0]
-            return pub[64:128] + "\n"
+            return generate_serval_keys(node.name)["sid"]
 
 
 def load_services():
